96ba1efa11ae0c15d78e69201e994fe2729223ab
Travis <travis@travis-ci.org>
2015-12-16 16:05:10 +0100

    init

---
+++ app/presenters/templates/Admin/HomepagePresenter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Admin\Presenters;
+
+use Nette;
+use App\Model;
+
+
+class HomepagePresenter extends BasePresenter
+{
+
+	public function renderDefault()
+	{
+		$this->template->anyVariable = 'any value';
+	}
+
+}
