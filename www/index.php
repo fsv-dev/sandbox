543803fb13fe8ae1187733a480c6991a3dc6475e
Travis <travis@travis-ci.org>
2015-12-28 08:34:07 +0100

    index: added condition for maintenance during deployment

--- www/index.php
+++ www/index.php
@@ -3,6 +3,11 @@
 // Uncomment this line if you must temporarily take down your site for maintenance.
 // require __DIR__ . '/.maintenance.php';
 
+// For deployment
+if(file_exists(__DIR__ . '/../../.htdeployment.running')){
+	require __DIR__ . '/.maintenance.php';
+}
+
 $container = require __DIR__ . '/../app/bootstrap.php';
 
 $container->getByType('Nette\Application\Application')->run();
